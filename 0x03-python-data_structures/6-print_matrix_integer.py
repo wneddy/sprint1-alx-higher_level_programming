902b1042a281d0c63cab37bc0f324819536f7deb
wneddy <wnedward23@gmail.com>
2023-12-04 05:00:24 -0800

    task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for entity in range(len(matrix)):
-        for figure in range(len(matrix[elem])):
+        for figure in range(len(matrix[entity])):
             if (figure != 0):
                 print(end=" ")
             print("{:d}".format(matrix[entity][figure]), end="")

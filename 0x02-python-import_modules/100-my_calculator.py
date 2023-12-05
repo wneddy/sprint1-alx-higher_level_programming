e2b5583c79f8642f5570efaca908f90f8e5e9928
wneddy <wnedward23@gmail.com>
2023-12-05 10:30:13 -0800

    task 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,18 +8,18 @@ if __name__ == "__main__":
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
 
-    e = int(argv[1])
-    d = int(argv[3])
+    a = int(argv[1])
+    b = int(argv[3])
     oprt = argv[2]
 
     if (operator == "+"):
-        print("{:d} {} {:d} = {:d}".format(e, oprt, d, calc.add(e, d)))
+        print("{:d} {} {:d} = {:d}".format(a, oprt, b, calc.add(a, b)))
     elif (oprt == "-"):
-        print("{:d} {} {:d} = {:d}".format(e, oprt, d, calc.sub(e, d)))
+        print("{:d} {} {:d} = {:d}".format(a, oprt, b, calc.sub(a, b)))
     elif (oprt == "*"):
-        print("{:d} {} {:d} = {:d}".format(e, oprt, d, calc.mul(e, d)))
+        print("{:d} {} {:d} = {:d}".format(a, oprt, b, calc.mul(a, b)))
     elif (oprt == "/"):
-        print("{:d} {} {:d} = {:d}".format(e, oprt, d, calc.div(e, d)))
+        print("{:d} {} {:d} = {:d}".format(a, oprt, b, calc.div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
         exit(1)
